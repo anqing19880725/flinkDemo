2fd87e312d5fb9e34c98406c65be125edc423d77
anqing19880725 <181562207@qq.com>
2021-08-10 22:37:14 +0800

    2021.8.10

--- src/main/java/com/atguigu/apitest/window/WindowTest1_TimeWindow.java
+++ src/main/java/com/atguigu/apitest/window/WindowTest1_TimeWindow.java
@@ -1,11 +1,21 @@
 package com.atguigu.apitest.window;
 
 import com.atguigu.apitest.beans.SensorReading;
+import org.apache.commons.collections.IteratorUtils;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
 public class WindowTest1_TimeWindow {
 
@@ -14,29 +24,65 @@ public class WindowTest1_TimeWindow {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        // 从文件读取数据
-        DataStream<String> inputStream = env.readTextFile("D:\\idea_workspace\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
+//        // 从文件读取数据
+//        DataStream<String> inputStream = env.readTextFile("D:\\idea_workspace\\FlinkTutorial\\src\\main\\resources\\sensor.txt");
+
+        // socket文本流
+        DataStream<String> inputStream = env.socketTextStream("localhost", 7777);
+
         DataStream<SensorReading> dataStream = inputStream.map(line -> {
             String[] fields = line.split(",");
             return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
         });
 
         // 开窗测试
-        dataStream.keyBy("id")
+
+        // 1.增量聚合函数
+        DataStream<Integer> resultStream = dataStream.keyBy("id")
 //                .countWindow(0,2);
 //                .window(EventTimeSessionWindows.withGap(Time.minutes(1)));
-                .timeWindow(Time.seconds(15));
+                .timeWindow(Time.seconds(15))
 //                .window(TumblingProcessingTimeWindows.of(Time.seconds(15)))
+                .aggregate(new AggregateFunction<SensorReading, Integer, Integer>() {
+                    @Override
+                    public Integer createAccumulator() {
+                        return 0;
+                    }
+
+                    @Override
+                    public Integer add(SensorReading value, Integer accumulator) {
+                        return accumulator + 1;
+                    }
+
+                    @Override
+                    public Integer getResult(Integer accumulator) {
+                        return accumulator;
+                    }
+
+                    @Override
+                    public Integer merge(Integer a, Integer b) {
+                        return a + b;
+                    }
+                });
 
 
+        // 2.全窗口函数
+        SingleOutputStreamOperator<Tuple3<String,Long,Integer>> resultStream2 = dataStream.keyBy("id")
+                .timeWindow(Time.seconds(15))
+//                .process(new ProcessWindowFunction<SensorReading, Object, Tuple, TimeWindow>() {
+//                })
+                .apply(new WindowFunction<SensorReading, Tuple3<String,Long,Integer>, Tuple, TimeWindow>() {
+                    @Override
+                    public void apply(Tuple tuple, TimeWindow window, Iterable<SensorReading> input, Collector<Tuple3<String,Long,Integer>> out) throws Exception {
+                        String id = tuple.getField(0);
+                        Long windowEnd = window.getEnd();
+                        Integer count = IteratorUtils.toList(input.iterator()).size();
+                        out.collect(new Tuple3<>(id,windowEnd,count));
+                    }
+                });
 
-
-
-
-
-
-
-
+//        resultStream.print();
+        resultStream2.print();
         env.execute();
 
 
